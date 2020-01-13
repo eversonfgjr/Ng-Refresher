319ce999ab706869a068425ec0e4b1790774fa13
Everson Junior <eversonfgjr@gmail.com>
2020-01-13 10:51:11 -0200

    27. Local References

--- src/app/persons/persons-input/persons-input.component.ts
+++ src/app/persons/persons-input/persons-input.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class PersonsInputComponent {
 
-
+  onCreatePerson(personName: string) {
+    console.log('Person created: ' + personName);
+  }
 
 }
