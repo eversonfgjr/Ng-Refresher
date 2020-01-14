c7c85b2629b2df82d5b57bc0f6c069739d1f4114
Everson Junior <eversonfgjr@gmail.com>
2020-01-14 09:38:04 -0200

    31. Setting Up Services

---
+++ src/app/persons/persons.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonsService {
+
+  constructor() { }
+
+  persons: string[] = ['Max', 'Manuel', 'Anna'];
+
+  addPerson(name: string) {
+    this.persons.push(name);
+  }
+}
